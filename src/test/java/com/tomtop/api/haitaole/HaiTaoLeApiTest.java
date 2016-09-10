a7b779df94140b3722036d2f3b61c85bbbe14aea
eyihcn <526048537@qq.com>
2016-09-10 17:29:19 +0800

    init

---
+++ src/test/java/com/tomtop/api/haitaole/HaiTaoLeApiTest.java
@@ -0,0 +1,11 @@
+package com.tomtop.api.haitaole;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class HaiTaoLeApiTest extends AbstractJUnit4SpringContextTests {
+
+}
